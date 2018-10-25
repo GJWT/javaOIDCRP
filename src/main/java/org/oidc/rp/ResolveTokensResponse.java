0abf9913bb2a0820d1d26f56e2f57b60857e615b
janne lauros <janne.lauros@gmail.com>
2018-10-25 18:43:00 +0300

    initial commit

---
+++ src/main/java/org/oidc/rp/ResolveTokensResponse.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (C) 2018 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.oidc.rp;
+
+import org.oidc.msg.oidc.IDToken;
+
+/** Class wrapping RP Handler resolve tokens response. */
+class ResolveTokensResponse {
+  
+  /** Verified id token. */
+  final IDToken idToken;
+  /** Access token. */
+  final String accessToken;
+
+  /**
+   * Constructor.
+   * 
+   * @param idToken
+   *          Verified id token.
+   * @param accessToken
+   *          access token.
+   */
+  ResolveTokensResponse(IDToken idToken, String accessToken) {
+    this.idToken = idToken;
+    this.accessToken = accessToken;
+  }
+
+  /**
+   * Get verified id token.
+   * 
+   * @return verified id token
+   */
+  public IDToken getIDToken() {
+    return idToken;
+  }
+
+  /**
+   * Get access token.
+   * 
+   * @return access token
+   */
+  public String getAccessToken() {
+    return accessToken;
+  }
+}
